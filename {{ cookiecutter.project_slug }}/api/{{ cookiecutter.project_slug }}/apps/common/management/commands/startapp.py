bd40e6a583ae09437e2bc5972a5cd1a82cce1c88
Andrey Butenko <whitespysoftware@gmail.com>
2022-12-15 04:50:47 +0200

    Add overriden startapp command to create app in apps directory

---
+++ {{ cookiecutter.project_slug }}/api/{{ cookiecutter.project_slug }}/apps/common/management/commands/startapp.py
@@ -0,0 +1,20 @@
+import os
+
+from django.conf import settings
+from django.core.management.base import CommandError
+from django.core.management.commands.startapp import Command as StartappCommand
+
+
+class Command(StartappCommand):
+    def handle(self, **options):
+        if options["directory"] is None:
+            app_directory = os.path.join(settings.BASE_DIR, "apps", options["name"])
+
+            try:
+                os.makedirs(app_directory, exist_ok=True)
+            except FileExistsError:
+                raise CommandError(f"'{app_directory}' already exists")  # pylint: disable=raise-missing-from
+            else:
+                options["directory"] = app_directory
+
+        super().handle(**options)
