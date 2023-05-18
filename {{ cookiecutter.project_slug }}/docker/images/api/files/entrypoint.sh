8b1e1dbad3eb0fe0022f98d046acb9c724b37ab1
Sergey Kubrak <sergey.kubrak@djangostars.com>
2023-05-18 12:44:28 +0300

    Updated docker entrypoint.

--- {{ cookiecutter.project_slug }}/docker/images/api/files/entrypoint.sh
+++ {{ cookiecutter.project_slug }}/docker/images/api/files/entrypoint.sh
@@ -16,14 +16,14 @@ start = time.time()
 while True:
     try:
         con = psycopg.connect(conninfo="${{ '{' }}{{ cookiecutter.__env_prefix }}DATABASE_URL}")
-        con.close()
-        break
     except psycopg.OperationalError as error:
         sys.stderr.write("Waiting for PostgreSQL to become available...\n")
 
         if time.time() - start > suggest_unrecoverable_after:
             sys.stderr.write("  This is taking longer than expected. The following exception may be indicative of an unrecoverable error: '{}'\n".format(error))
-
+    else:
+        con.close()
+        break
     time.sleep(1)
 END
 
